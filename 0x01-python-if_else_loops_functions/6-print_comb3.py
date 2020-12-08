c1f451dc5307fa9313c6d201ddccd9f717cc0f87
david-kimengu <davidmutune200@gmail.com>
2020-12-08 17:06:46 +0000

    Print possible combos of  2 numbers

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,6 +4,8 @@ for i in range(0, 90):
     l_digit = number % 10
     f_digit = number // 10
 
-    if (f_digit < l_digit):
-        print("{}{}".format(f_digit, l_digit))
+    if (f_digit < l_digit and number < 89):
+        print("{}{}".format(f_digit, l_digit), end=", ")
+    elif (number == 89):
+        print(89)
     number += 1
