255fbd36117af02dca8b9f4c7ae3c2d347456c39
david-kimengu <davidmutune200@gmail.com>
2020-12-15 19:17:02 +0000

    Remove pep8 error

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     bool_list = []
     for i in my_list:
         if i % 2:
-            bool_list =bool_list + [False]
+            bool_list = bool_list + [False]
         else:
             bool_list = bool_list + [True]
     return bool_list
