dd0f3716c272714c29c57e6b64a195610f69b95f
david-kimengu <davidmutune200@gmail.com>
2021-03-01 19:53:15 +0000

    Remove syntax error

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,5 @@
- -- Lists all records of the table second_table in descending order.
+-- Lists all records of the table second_table
+-- in descending order
 SELECT `score`, `name`
 FROM `second_table`
 ORDER BY `score` DESC;
