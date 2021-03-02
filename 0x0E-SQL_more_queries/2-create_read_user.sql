7bd67573af78fc75576a9040a269343724796e99
david-kimengu <davidmutune200@gmail.com>
2021-03-02 20:03:48 +0000

    Remove style error

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
- -- create databse and adds a new user
+-- create databse and adds a new user
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS user_0d_2@localhost
 IDENTIFIED BY 'user_0d_2_pwd';
