54b63425d6a4ac72394f83fc7b237a18ae810870
kimengu-david <davidmutune200@gmail.com>
2021-03-29 19:13:07 +0000

    Add module documentation

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-# Lists all State objects from the database hbtn_0e_6_usa.
-
+"""
+Lists all State objects from the database hbtn_0e_6_usa.
+usage <username> <passwd> <db name>
+"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
