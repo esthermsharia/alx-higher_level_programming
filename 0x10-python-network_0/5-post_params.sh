19d271788674b4555cc3be320cc208607339e241
kimengu-david <davidmutune200@gmail.com>
2021-04-11 20:19:38 +0000

    Send POST reqeust with curl

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL as an argument, sends a POST request tothe passed URL, and displays the body of the response.
-curl -sL "$1" -X POST -d "email=hr@hr@holbertonschool.com&subject=I will always be here for PLD"
+curl -s -X POST -d "email=hr@hr@holbertonschool.com&subject=I will always be here for PLD" "$"
