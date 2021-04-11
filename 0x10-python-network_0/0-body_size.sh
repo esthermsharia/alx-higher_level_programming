2d79fa33376598f5f1711ce62d7afff6295c6d2a
kimengu-david <davidmutune200@gmail.com>
2021-04-11 16:38:59 +0000

    Add task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Takes in a URL, sends a request to that URL,
 # displays the size of the body of the response.
-curl -s "$1" | grep "Content-Length:"| cut -d"" -f2
+curl -s "$1" | grep "Content-Length:"| cut -d" " -f2
