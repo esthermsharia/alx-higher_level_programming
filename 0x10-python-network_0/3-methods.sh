83addf82172b5a303865266ed683ff8da6552aae
kimengu-david <davidmutune200@gmail.com>
2021-04-11 19:13:20 +0000

    Cut from byte no 8-

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL and displays all HTTP methods the server will accept.
-curl -sI "$1" -X OPTIONS| grep "Allow:" | cut -d':' -f2 
+curl -sI "$1" -X OPTIONS|grep Allow|cut -b 8-
