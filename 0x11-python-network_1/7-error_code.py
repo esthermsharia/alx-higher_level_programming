5cd885c18fd1c26559e95611f43ef744c42325ae
kimengu-david <davidmutune200@gmail.com>
2021-04-15 16:14:48 +0000

    Remove pep8 style error

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Takes in a URL, sends a request to the URL and displays the body of the response.
+Takes in a URL, sends a request to the URL and displays
+the body of the response.
 """
 import sys
 import requests
